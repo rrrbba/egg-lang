79ff106c89d6661f3d9a3f1fbaabd3f8f08acf6e
Rayven <rayven.burns@gmail.com>
2021-07-07 19:59:38 -0400

    added parseapply

--- parser.js
+++ parser.js
@@ -29,3 +29,30 @@ const skipSpace = (string) => {
     if (first == -1) return "";
     return string.slice(first);
 }
+
+const parseApply = (expr, program) => {
+    program = skipSpace(program);
+    if (program[0] != "(") {
+        return {expr: expr, rest: program};
+    }
+
+    program = skipSpace(program.slice(1));
+    expr = {type: "apply", operator: expr, args: []};
+
+    while(program[0] != ")") {
+        let arg = parseExpression(program);
+
+        expr.args.push(arg.expr);
+        
+        program = skipSpace(arg.rest);
+
+        if(program[0] == ",") {
+            program = skipSpace(program.slice(1));
+        } else if (program[0] != ")"){
+            throw new SyntaxError("Expected ',' or ')'");
+        }
+    }
+
+    return parseApply(expr, program.slice(1));
+
+}
